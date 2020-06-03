966f2149314a11b251acfb933e908449961ea570
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-03 18:40:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,9 +2,9 @@
 require "pry"
 def snake_it_up(string)
   if string[0] == "s"
-    new_num = 
+    new_num = 10.to_s 
     binding.pry
-    10.to_s * "s" + string
+    new_num * "s" + string
   else
     string
   end
